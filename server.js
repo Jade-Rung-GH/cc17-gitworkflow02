bee672a2df80914d1c373d45628e99d66f5357f2
playvolley <ratchanon_cu@outlook.com>
2024-06-19 12:24:27 +0700

    AddErrorMiddleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const errorMiddleware = require("./middlewares/error-middleware");
 
 const app = express();
 
+app.use(errorMiddleware);
 let port = process.env.PORT || 8000;
 app.listen(port, () => console.log("Server is on port", port));
